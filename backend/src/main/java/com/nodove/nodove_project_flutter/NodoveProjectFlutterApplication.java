f53e3ad016f9a393f6e84ccc4e871c71aa18bb31
choisimo <150008602+choisimo@users.noreply.github.com>
2024-04-02 09:50:07 +0900

    add frontend && backend

---
+++ backend/src/main/java/com/nodove/nodove_project_flutter/NodoveProjectFlutterApplication.java
@@ -0,0 +1,13 @@
+package com.nodove.nodove_project_flutter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodoveProjectFlutterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodoveProjectFlutterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nodove/nodove_project_flutter/NodoveProjectFlutterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nodove.nodove_project_flutter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodoveProjectFlutterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
